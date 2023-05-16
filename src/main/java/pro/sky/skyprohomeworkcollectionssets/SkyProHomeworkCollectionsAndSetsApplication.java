ecbe1a8b440d1129c7759941b44155a6480513a4
Smokvin <alex.smokvin@gmail.com>
2023-05-16 19:59:02 +0300

    initial commit

---
+++ src/main/java/pro/sky/skyprohomeworkcollectionssets/SkyProHomeworkCollectionsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprohomeworkcollectionssets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProHomeworkCollectionsAndSetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProHomeworkCollectionsAndSetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/skyprohomeworkcollectionssets/SkyProHomeworkCollectionsAndSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprohomeworkcollectionssets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProHomeworkCollectionsAndSetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
